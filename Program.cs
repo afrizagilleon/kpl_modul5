fb9b50b895bcb21e8998001b00b9c5c8a8ccd654
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 07:12:46 +0700

    implementasi dengan generic method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace modul5_103022300154
+{
+    class Program
+    {
+        public static void Main()
+        {
+            PemrosesData pd = new PemrosesData();
+
+            double maxVal = pd.DapatkanNilaiTerbesar<double>(10.0, 30.2, 22.1);
+            Console.WriteLine($"Nilai Max: {maxVal}" );
+        }
+    }
+     class PemrosesData
+    {
+        public T DapatkanNilaiTerbesar<T>(T a, T b, T c)
+        {
+            dynamic max = a;
+            if( (dynamic) b > max)
+            {
+                max = b;
+            }
+            
+            if((dynamic) c > max)
+            {
+                max = c;
+            }
+            return max;
+        }
+    }
+}
